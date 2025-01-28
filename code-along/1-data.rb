d47489c6cff94b8734bb3d98b391e5a65543749a
nishant kumar <nishant.kumar@kellogg.northwestern.edu>
2025-01-28 20:04:19 +0000

    ruby intro

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -6,14 +6,56 @@
 
 # Numbers
 
+
+puts 2
+puts 5
+
+puts 5 + 2
+puts 5.0 / 2
+puts 5 * 2
+puts 5 - 2
+
+puts (5 + 2)*5
 # Non-numbers
+
+puts "Hello World!!"
+
+puts "Hello" + " World!!"
+
+puts "tacos " * 3
+
+puts "tacos:"+ 3.to_s
 
 # True, False
 
+puts true
+puts false
+
 # Nothing
+
+puts nil
 
 # Variables
 
+food = "tacos"
+qty = 3
+puts food * qty
+
 # Combine strings and variables
 
+puts  "tacos: #{qty}"
+
+puts "#{food}: #{qty}"
+
+
 # String manipulation
+
+puts "Hello".length
+puts "How are you".capitalize
+puts "computer".reverse
+
+creed = "This Is The Way"
+
+puts creed.upcase
+puts creed.downcase
+puts creed.swapcase
